2507cf4b6bf26d4da435ccb5bae577f1c36877c1
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-02-13 01:38:27 +0530

    SortedArraySearch.java

---
+++ Day1/SortedArraySearch.java
@@ -0,0 +1,16 @@
+class SortedArraySearch {
+    static boolean searchInSorted(int arr[], int k) {
+        // Your code here
+        int n = arr.length;
+        for(int i = 0;i<n;i++){
+            if(arr[i] == k){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// Geeksforgeeks
+// Striver LinearSearch Question
+// Time Complexity: O(n)
+// Space Complexity : O(1)
