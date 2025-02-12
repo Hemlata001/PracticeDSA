49ae3d1ec90cd3ea17485229c8bfb36658e86702
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-02-12 21:27:51 +0530

    Add files via upload

---
+++ Day1/Reverse.java
@@ -0,0 +1,18 @@
+package Day1;
+import java.util.Arrays;
+public class Reverse {
+    public static void reverse(int[] arr){
+        int n = arr.length-1;
+        for(int i = 0;i<n/2;i++){
+            int temp = arr[i];
+            arr[i] = arr[n-i];
+            arr[n-i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        System.out.println("Before reversing: " + Arrays.toString(arr));
+        reverse(arr);
+        System.out.println("Before reversing: " + Arrays.toString(arr));
+    }
+}
