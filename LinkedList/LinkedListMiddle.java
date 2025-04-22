2708631ed4d25ef05c0a820aa28b0d06856dd4b9
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-04-23 00:06:50 +0530

    LinkedListMiddle.java

---
+++ LinkedList/LinkedListMiddle.java
@@ -0,0 +1,25 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class LinkedListMiddle {
+    public static ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow; // Middle node
+    }
+}
+
+// time Complexity : O(n)
+// space Complexity : O(1)
